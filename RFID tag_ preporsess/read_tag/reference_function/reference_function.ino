4f1e6e2f05aefd156f71e3fcefb84a52de5d6640
luying <ring910224@gmail.com>
2024-07-25 09:07:00 +0800

    add RFID read  function

---
+++ RFID tag_ preporsess/read_tag/reference_function/reference_function.ino
@@ -0,0 +1,78 @@
+void MFRC522::PICC_DumpToSerial(Uid *uid) {
+	MIFARE_Key key;
+	
+	// Dump UID, SAK and Type
+	PICC_DumpDetailsToSerial(uid);
+	
+	// Dump contents
+	PICC_Type piccType = PICC_GetType(uid->sak);
+	switch (piccType) {
+		case PICC_TYPE_MIFARE_MINI:
+		case PICC_TYPE_MIFARE_1K:
+		case PICC_TYPE_MIFARE_4K:
+			// All keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+			for (byte i = 0; i < 6; i++) {
+				key.keyByte[i] = 0xFF;
+			}
+			PICC_DumpMifareClassicToSerial(uid, piccType, &key);
+			break;
+			
+		case PICC_TYPE_MIFARE_UL:
+			PICC_DumpMifareUltralightToSerial();
+			break;
+			
+		case PICC_TYPE_ISO_14443_4:
+		case PICC_TYPE_MIFARE_DESFIRE:
+		case PICC_TYPE_ISO_18092:
+		case PICC_TYPE_MIFARE_PLUS:
+		case PICC_TYPE_TNP3XXX:
+			Serial.println(F("Dumping memory contents not implemented for that PICC type."));
+			break;
+			
+		case PICC_TYPE_UNKNOWN:
+		case PICC_TYPE_NOT_COMPLETE:
+		default:
+			break; // No memory dump here
+	}
+	
+	Serial.println();
+	PICC_HaltA(); // Already done if it was a MIFARE Classic PICC.
+} // End PICC_DumpToSerial()
+
+
+void MFRC522::PICC_DumpMifareClassicToSerial(	Uid *uid,			///< Pointer to Uid struct returned from a successful PICC_Select().
+												PICC_Type piccType,	///< One of the PICC_Type enums.
+												MIFARE_Key *key		///< Key A used for all sectors.
+											) {
+	byte no_of_sectors = 0;
+	switch (piccType) {
+		case PICC_TYPE_MIFARE_MINI:
+			// Has 5 sectors * 4 blocks/sector * 16 bytes/block = 320 bytes.
+			no_of_sectors = 5;
+			break;
+			
+		case PICC_TYPE_MIFARE_1K:
+			// Has 16 sectors * 4 blocks/sector * 16 bytes/block = 1024 bytes.
+			no_of_sectors = 16;
+			break;
+			
+		case PICC_TYPE_MIFARE_4K:
+			// Has (32 sectors * 4 blocks/sector + 8 sectors * 16 blocks/sector) * 16 bytes/block = 4096 bytes.
+			no_of_sectors = 40;
+			break;
+			
+		default: // Should not happen. Ignore.
+			break;
+	}
+	
+	// Dump sectors, highest address first.
+	if (no_of_sectors) {
+		Serial.println(F("Sector Block   0  1  2  3   4  5  6  7   8  9 10 11  12 13 14 15  AccessBits"));
+		for (int8_t i = no_of_sectors - 1; i >= 0; i--) {
+			PICC_DumpMifareClassicSectorToSerial(uid, key, i);
+		}
+	}
+	PICC_HaltA(); // Halt the PICC before stopping the encrypted session.
+	PCD_StopCrypto1();
+} // End PICC_DumpMifareClassicToSerial()
+
